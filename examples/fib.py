4abed4e8eeec39abbba85cc4c832ae5eeb69b0e7
vext01 <vext01@gmail.com>
2013-05-30 14:34:42 +0100

    Add fibonacci example and reference implementation

---
+++ examples/fib.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.7
+import sys
+
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n-1) + fib(n-2)
+
+if __name__ == "__main__":
+    print(fib(int(sys.argv[1])))
