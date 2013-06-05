205cba14c196880b9c677a76e5721a7c12a4f485
vext01 <vext01@gmail.com>
2013-06-05 14:31:43 +0100

    suck in from file in an RPythonic way

--- target_zzz80.py
+++ target_zzz80.py
@@ -1,23 +1,19 @@
 import sys
+import os
 import parse, zzz80
+from util import bail
 
-SRC = """
-POP r1
-MOV r2, 0
-loop:
-    SUB r1, 1
-    ADD r2, 1
-    JE end, r1, 0
-    JMP loop
-
-end:
-    DUMP
-"""
+from rpython.rlib.streamio import open_file_as_stream
 
 def main(argv):
 
-    prog = parse.parse(SRC)
-    prog.run([int(argv[1])])
+    if len(argv) < 2:
+        bail("bad usage")
+
+    hndl = open_file_as_stream(argv[1])
+    prog = parse.parse(hndl.readall())
+    hndl.close()
+    prog.run([int(x) for x in argv[2:]])
 
     return 0
 
