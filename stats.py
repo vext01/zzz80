1b9965c0f4ee831686d575b9515763a7bf60f70f
vext01 <vext01@gmail.com>
2013-06-07 08:27:27 +0100

    generate some graphs

--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 import time, os, sys
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-NUM_TESTS = 35
+ENV = "PYTHONPATH=/home/edd/source/pypy"
 
 def time_exec(cmd):
     t0 = time.time()
@@ -10,13 +10,25 @@ def time_exec(cmd):
     return t1 - t0
 
 if __name__ == "__main__":
+    #times_cpy = []
+    #times_pypy = []
+
+    if len(sys.argv) < 2:
+        print("specify num tests")
+        sys.exit(1)
+
+    NUM_TESTS = int(sys.argv[1])
     times_nojit = []
     times_jit = []
     for tno in xrange(NUM_TESTS):
         print("running test #%s" % tno)
+        #cpy = time_exec("%s python target_zzz80.py examples/fib.zzz %s" % (ENV, tno))
+        #pypy = time_exec("%s pypy target_zzz80.py examples/fib.zzz %s" % (ENV, tno))
         nojit = time_exec("./bin/nojit examples/fib.zzz %s" % tno)
         jit = time_exec("./bin/jit examples/fib.zzz %s" % tno)
 
+        #times_cpy.append(cpy)
+        #times_pypy.append(pypy)
         times_nojit.append(nojit)
         times_jit.append(jit)
 
@@ -25,19 +37,26 @@ if __name__ == "__main__":
     print(times_jit)
 
     from prettytable import PrettyTable
-    t = PrettyTable(["FIB #", "No Jit (s)", "Jit (s)"])
+    #t = PrettyTable(["FIB #", "CPython", "PyPy", "No Jit", "Jit"])
+    t = PrettyTable(["FIB #", "No Jit", "Jit"])
     t.align[1] = "r"
     t.align[2] = "r"
+    #t.align[3] = "r"
+    #t.align[4] = "r"
 
     for i in xrange(NUM_TESTS):
+        #t.add_row([i, times_cpy[i], times_pypy[i], times_nojit[i], times_jit[i]])
         t.add_row([i, times_nojit[i], times_jit[i]])
 
     print(t)
 
-    #plt.plot([10**x for x in xrange(NUM_TESTS)], times_nojit, "ro")
-    #plt.plot([10**x for x in xrange(NUM_TESTS)], times_jit, "go")
-    #plt.xlabel("loop iterations")
-    #plt.ylabel("time (s)")
-    #plt.show()
+    #plt.plot([x for x in xrange(NUM_TESTS)], times_cpy)
+    #plt.plot([x for x in xrange(NUM_TESTS)], times_pypy)
+    plt.plot([x for x in xrange(NUM_TESTS)], times_nojit, label="No JIT", marker="x", linestyle="-")
+    plt.plot([x for x in xrange(NUM_TESTS)], times_jit, label="JIT", marker="x", linestyle="-")
+    plt.xlabel("FIB Number")
+    plt.ylabel("time (s)")
+    plt.legend()
+    plt.show()
 
 
