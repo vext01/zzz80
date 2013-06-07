0c455296787faaf04e826c51d767b4b3bbda6050
vext01 <vext01@gmail.com>
2013-06-07 16:17:48 +0100

    comment target

--- target_zzz80.py
+++ target_zzz80.py
@@ -1,3 +1,15 @@
+# This is the entry point for rpython to begin its analysis and
+# translation; the so-called 'target'.
+#
+# The function "target" should return a function at which rpython
+# should begins it's analysis.
+#
+# A function 'jitpolicy' should return a JitPolicy. The default one
+# is fine in this instance.
+#
+# This can also be run by normal cpython or pypy for testing purposes,
+# just so long as the rpython libs are available.
+
 import sys
 import os
 import parse
@@ -7,14 +19,13 @@ from rpython.rlib.streamio import open_file_as_stream
 
 def main(argv):
 
-    if len(argv) < 2:
-        bail("bad usage")
+    if len(argv) < 2: bail("bad usage")
 
     hndl = open_file_as_stream(argv[1])
     prog = parse.parse(hndl.readall())
     hndl.close()
 
-    args = [ int(x) for x in argv[2:]]
+    args = [ int(x) for x in argv[2:] ]
     prog.run(args)
 
     return 0
@@ -28,4 +39,3 @@ def jitpolicy(driver):
 
 if __name__ == "__main__":
     main(sys.argv)
-
